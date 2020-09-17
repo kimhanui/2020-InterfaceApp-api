9b910104a393a41b5b8f15eac4e4b7956105836d
LAPTOP-0I0SFG2M\kimhanui <cocacola2017@naver.com>
2020-09-17 22:47:16 +0900

    [CLN] airbrake썼던거 import해서 에러남. 지움

--- src/main/java/com/infe/app/AppApplication.java
+++ src/main/java/com/infe/app/AppApplication.java
@@ -1,6 +1,5 @@
 package com.infe.app;
 
-import io.airbrake.javabrake.Notifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
