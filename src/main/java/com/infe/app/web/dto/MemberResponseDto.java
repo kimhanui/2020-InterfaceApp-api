b5a2d5e53b98a20d384dd3242f8ff366fa6b6e10
LAPTOP-0I0SFG2M\kimhanui <cocacola2017@naver.com>
2020-11-14 01:31:09 +0900

    [CLN] dev.properties에 naming 옵션 추가, memberDTO에 contact필드 추가

--- src/main/java/com/infe/app/web/dto/MemberResponseDto.java
+++ src/main/java/com/infe/app/web/dto/MemberResponseDto.java
@@ -9,11 +9,13 @@ public class MemberResponseDto {
     private Long studentId;
     private String name;
     private Long groupNum;
+    private String contact;
 
     public MemberResponseDto(Member m) {
         this.id = m.getId();
         this.studentId = m.getStudentId();
         this.name = m.getName();
         this.groupNum = m.getGroupNum();
+        this.contact = m.getContact();
     }
 }
